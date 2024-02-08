d44f11891d85f17f41fbf30b03b35922df56583b
Harlyn Luna <91364909+HJosueLB@users.noreply.github.com>
2024-02-07 23:20:20 -0600

    Tarea #02 | SC-403 | Desarrollo de Aplicaciones Web y Patrones Se finaliza desarrollo FrontEnd del portafolio digital personal Harlyn  Luna, el proyecto continuara en desarrollo dado que se desea publicar  el mismo.

---
+++ src/main/java/com/tarea2/demo/HlbPorfolioApplication.java
@@ -0,0 +1,13 @@
+package com.tarea2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlbPorfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlbPorfolioApplication.class, args);
+	}
+
+}
